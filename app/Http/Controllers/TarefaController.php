5791befeeb28f66a14f3ca138adbaf30d228b472
Hiago PC <hiagok.o@gmail.com>
2025-04-28 22:06:09 -0300

    att backend

---
+++ app/Http/Controllers/TarefaController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Enum\StatusTarefa;
+use App\Models\Tarefa;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+class TarefaController extends Controller
+{
+    public function index()
+    {
+        return Tarefa::all()
+            ->map( function (Tarefa $tarefa) {
+                return [
+                    'id' => $tarefa->id,
+                    'titulo' => $tarefa->titulo,
+                    'descricao' => $tarefa->descricao,
+                    'data_vencimento' => $tarefa->data_vencimento,
+                    'status' => $tarefa->status,
+                ];
+            });
+    }
+
+    public function create()
+    {
+        return view('tarefas.create');
+    }
+
+    public function store(Request $request)
+    {
+        // Lógica para armazenar a tarefa no banco de dados
+        $data = $request->validate([
+            'titulo' => 'required|string|max:255',
+            'descricao' => 'required',
+            'data_vencimento' => 'required|date',
+            'status' => [Rule::enum(StatusTarefa::class)]// ainda nao entendi como validar enum
+        ]);
+
+        $novaTarefa = Tarefa::create($data);
+        
+        return response($novaTarefa, 201);
+    }
+
+    public function edit(Tarefa $tarefa)
+    {
+        return response($tarefa, 201);
+    }
+
+    public function update(Tarefa $tarefa, Request $request)
+    {
+        // Lógica para atualizar a tarefa no banco de dados
+        $data = $request->validate([
+            'titulo' => 'required|string|max:255',
+            'descricao' => 'required',
+            'data_vencimento' => 'required|date',
+            'status' => [Rule::enum(StatusTarefa::class)],// ainda nao entendi como validar enum
+        ]);
+
+        $tarefa->update($data);
+
+        return response($tarefa, 201);
+    }
+
+    public function destroy(Tarefa $tarefa)
+    {
+        // Lógica para excluir a tarefa do banco de dados
+        $tarefa->delete();
+        return response('Deletada com sucesso', 201);
+    }
+
+    // public function show($id)
+    // {
+    //     return view('tarefas.show', compact('id'));
+    // }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,24 @@
 <?php
 
+use App\Http\Controllers\TarefaController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
+
+// Routes importadas do repostitório de teste
+
+Route::get('/tarefas', [TarefaController::class, 'index'])->name('tarefas.index');
+
+Route::get('/tarefas/create', [TarefaController::class, 'create'])->name('tarefas.create');
+
+Route::post('/tarefas', [TarefaController::class, 'store'])->name('tarefas.store');
+
+Route::get('/tarefas/{tarefa}/edit', [TarefaController::class, 'edit'])->name('tarefas.edit');
+
+Route::get('/tarefas/{tarefa}/update', [TarefaController::class, 'update'])->name('tarefas.update');
+
+Route::delete('/tarefas/{tarefa}/destroy', [TarefaController::class, 'destroy'])->name('tarefas.destroy');
 
 require __DIR__.'/auth.php';
